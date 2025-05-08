715c26cfad55b420a91f0c7a70ab0390e2293c05
Fabiano <radbios@gmail.com>
2025-05-08 05:01:15 -0300

    update

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class PaymentController extends Controller
+{
+    public function index(Request $request)
+    {
+        $validated = $request->validate([
+            "order_id" => ["required"]
+        ]);
+
+        $order_id = $validated["order_id"];
+
+        $response = Http::withHeaders([
+            "Authorization" => "Bearer " . $request->bearerToken()
+        ])->get(env("APP_GATEWAY") . '/order/order/' . $order_id);
+
+        $order = $response->json();
+        return response()->json([
+            "payment" => [
+                "link" => route("pay"),
+                "method" => "post",
+            ],
+            "order" => $order
+        ]);
+    }
+
+    public function pay(Request $request)
+    {
+        $validated = $request->validate([
+            "order_id" => ["required"]
+        ]);
+
+        $order_id = $validated["order_id"];
+
+        $response = Http::withHeaders([
+            "Authorization" => "Bearer " . $request->bearerToken()
+        ])->put(env("APP_GATEWAY") . '/order/order/' . $order_id);
+
+        $order = $response->json();
+
+        return response()->json([
+            "message" => "Pagamento efetuado com sucesso!",
+            "order" => $order
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Auth;
+use App\Http\Controllers\PaymentController;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("api_auth")->group(function() {
-    Route::get("test", function(Request $request) {
-        return Auth::user()->id;
-    });
+    Route::get("payment", [PaymentController::class, "index"]);
+    Route::post("payment", [PaymentController::class, "pay"])->name("pay");
 });
